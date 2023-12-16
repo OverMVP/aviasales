e3214d2b03ab866f4730886907dab6fcae3bd629
OverMVP <eugenefrosttv@gmail.com>
2023-12-16 18:06:21 +0300

    deleted cl

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ import Cookies from 'js-cookie';
 export default async function getSearchId(): Promise<any> {
   return fetch('https://aviasales-test-api.kata.academy/search')
     .then((res) => {
-      console.log(res);
       if (res.ok) return res.json();
       return getSearchId();
     })
